e7ad2746fb46fdababa473ee8c1a91b6146bb85a
TiveCS <creativecode.id@gmail.com>
2022-03-16 23:56:24 +0700

    menambahkan class DataGeneric

---
+++ tutor-modul4/DataGeneric.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tutor_modul4
+{
+	internal class DataGeneric<T>
+	{
+		private T data;
+
+		public DataGeneric(T data)
+		{
+			this.data = data;
+		}
+
+		public void PrintInfo()
+		{
+			Console.WriteLine("Data yang disimpan adalah: " + this.data);
+		}
+
+	}
+}
--- tutor-modul4/Program.cs
+++ tutor-modul4/Program.cs
@@ -10,7 +10,8 @@ namespace tutor_modul4
 	{
 		static void Main(string[] args)
 		{
-
+			DataGeneric<String> data = new DataGeneric<string>("1302204090");
+			data.PrintInfo();
 		}
 	}
 }
